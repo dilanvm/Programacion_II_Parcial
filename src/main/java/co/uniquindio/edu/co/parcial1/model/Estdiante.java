83d37f43392319392151540169cef675d5d67188
dilanvm <diland.ruizr@uqvirtual.edu.co>
2026-09-23 21:36:00 -0500

    variables de clase estudiante

--- src/main/java/co/uniquindio/edu/co/parcial1/model/Estdiante.java
+++ src/main/java/co/uniquindio/edu/co/parcial1/model/Estdiante.java
@@ -1,4 +1,14 @@
 package co.uniquindio.edu.co.parcial1.model;
 
+import java.util.List;
+
 public class Estdiante {
+    private String nombreCompleto;
+    private int numeroIdentidad;
+    private int telefono;
+    private String correoElectronico;
+    private int edad;
+    private String fechaRegistro;
+
+    private List<Profesor>
 }
