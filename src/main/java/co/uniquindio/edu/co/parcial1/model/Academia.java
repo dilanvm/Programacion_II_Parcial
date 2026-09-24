d7e6f49c9afd0c610b95bae87718d75840bf2131
SofiaPr79 <sofia.varelag@uqvirtual.edu.co>
2026-09-24 16:56:06 -0500

    Terminar academia

--- src/main/java/co/uniquindio/edu/co/parcial1/model/Academia.java
+++ src/main/java/co/uniquindio/edu/co/parcial1/model/Academia.java
@@ -2,7 +2,6 @@ package co.uniquindio.edu.co.parcial1.model;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Academia {
     public static Academia instancia;
     private String nombreComercial;
